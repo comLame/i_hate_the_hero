09c650892743685ebf2d66dbbe69797a38a1e16f
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-12-01 20:14:22 +0900

    【機能追加】ゲーム部分の機能を少し追加
    ・ボールの自動生成
    ・なぞって消す
    ・消したら生成

---
+++ i_hate_the_hero/Assets/Script/GameScene/TouchDrop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDrop : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	public Sprite[] spriteBall;
+
+	private GameObject firstBall;
+	private List<GameObject> removableBallList;
+	private GameObject lastBall;
+	private string currentName;
+
+	private void Update(){
+		if (Input.GetMouseButton(0) && firstBall == null) {
+			//ボールをドラッグしはじめたとき
+			OnDragStart();
+		} else if (Input.GetMouseButtonUp(0)) {
+			//ボールをドラッグし終わったとき
+			OnDragEnd();
+		} else if (firstBall != null) {
+			//ボールをドラッグしている途中
+			OnDragging();
+		}
+	}
+	
+	private void OnDragStart(){
+	}
+
+	private void OnDragEnd(){
+
+	}
+
+	private void OnDragging(){
+
+	}
+}
